2ef3d7fbe33bfac0c69e34c9a968ebdb16f5ba3b
dielduarte <magdiel.kesller@gmail.com>
2015-11-13 00:33:46 -0200

    finish layout

---
+++ app/editor/editor.routes.js
@@ -0,0 +1,33 @@
+(function(){
+  'use strict';
+// customers.routes.js
+angular
+    .module('editor')
+    .run(appRun);
+
+/* @ngInject */
+function appRun(routerHelper) {
+    routerHelper.configureStates(getStates(), '/hello');
+}
+
+function getStates() {
+    return [      
+        {
+            state: 'editor',
+            config: {
+                template: '',
+                url: '/editor',
+                abstract: true
+            }
+        },
+        {
+            state: 'editor-edit',
+            config: {
+                templateUrl: 'layout/editor/index.html',
+                url: '/editor'
+            }
+        }
+    ];
+}
+
+})();
